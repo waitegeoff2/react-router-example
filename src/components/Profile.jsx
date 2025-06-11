3b23947fb0f890d033a57fff1d4c8d848aee0c2c
Geoffrey Waite <186988887+waitegeoff2@users.noreply.github.com>
2025-06-11 11:13:14 -0400

    Added Outlet for different profiles

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,8 +1,13 @@
+import { Outlet } from "react-router-dom";
+
 const Profile = () => {
   return (
     <div>
       <h1>Hello from profile page!</h1>
       <p>So, how are you?</p>
+      <hr />
+      <h2>The profile visited is here:</h2>
+      <Outlet />
     </div>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App";
 import Profile from "./components/Profile";
+import Spinach from "./components/Spinach";
+import Popeye from "./components/Popeye";
 
 const router = createBrowserRouter([
   {
@@ -12,6 +14,10 @@ const router = createBrowserRouter([
   {
     path: "profile",
     element: <Profile />,
+    children: [
+      { path: "spinach", element: <Spinach /> },
+      { path: "popeye", element: <Popeye /> },
+    ]
   },
 ]);
 
